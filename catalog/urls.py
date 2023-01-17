902f5c088173033c9bf2769f92898117645911ec
Зубайраева Ольга <vadelevich2013@yandex.ru>
2023-01-17 12:25:47 +0300

    add all

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home,contacts
+from catalog.views import home,contacts # (*)
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('',home),
-    path('',contacts),
+    path('',home), # http://127.0.0.1:8000/home
+    path('contacts/',contacts),  #http://127.0.0.1:8000/home/contacts/
 ]
